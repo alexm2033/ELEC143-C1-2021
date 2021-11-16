a38130e135ce56d45d499541f926dc8950d3f40c
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2021-11-16 18:06:40 +0000

    Update main.cpp

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -12,7 +12,24 @@ int main()
     display.printf("Task 2");
     // ***** MODIFY THE CODE BELOW HERE *****
 
-    // Using a do-while-loop, count from 0 to +50 in steps of 5 - print the results on row 2 of the LCD screen every 1 second 
+    // Using a do-while-loop, count from 0 to +50 in steps of 5 - print the results on row 2 of the LCD screen every 1 second
+    
+    // define and initialise variable
+    int n = 00;
+
+   
+    //set up loop to count and display
+    do {
+        display.locate(1, 7);   //Row 1, Column 7 (in center of row)
+        display.printf("%d", n);    //print current value of n
+
+        n = n+5;    //add five to n and update value
+
+        wait_us(1000000);   //wait one second
+
+    } while (n<=50); //set condition to reenter loop
+
+    
 
     // ***** MODIFY THE CODE ABOVE HERE *****
     leds = 7;
