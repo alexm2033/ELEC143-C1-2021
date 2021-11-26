3a8857d734ab16082120aea0786e57b5606070e7
alexm2033 <alex.meredith@students.plymouth.ac.uk>
2021-11-26 18:37:08 +0000

    Update main.cpp

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -26,7 +26,8 @@ void displayArray(int rows, int cols, double *Array)
 }
 
 // ***** For (1) WRITE YOUR ARRAY Y HERE *****
-
+double Y[4][4] = {0};
+printf("%d\n", Y);
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 
 // *****  END YOUR FUNCTION HERE  *****
@@ -37,9 +38,24 @@ int main()
     displayArray(4,3,(double*)M);           //Display in terminal
 
     // ***** MODIFY THE CODE BELOW HERE *****
-
+int a, b, c, d, e, x, tempn[3], tempm[3], tempe[3];
     // 1. Create another Array Y with the correct dimensions to hold the result of M*N (see comments above)
+        for (a=0; a<1;) {
+            for (b=0; b<3; b++){
+                tempm[b] = M[a][b];
+            }
+        }
+            for ( c = 0; c<3; c++) {
+                for (d = 0; d<1; ) {
+                    tempn[c] = N[c][d];
+                }
+            }
 
+                for ( e = 0; e<3; e++) {
+                    tempe[e] = tempm[e] * tempn[e];
+                }
+                    Y[0][0] += tempe[e];
+                    printf("%f\n", Y[0][0]);
     // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
 
     // 3. Write a function to multiply two matrices together (see comments above). Include some text code to demonstrate it working
