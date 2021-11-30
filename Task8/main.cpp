f3eec15316fae9091980a652a63e12d1e66ed43d
alexm2033 <91289563+alexm2033@users.noreply.github.com>
2021-11-30 16:04:50 +0000

    Update main.cpp

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -26,75 +26,86 @@ void displayArray(int rows, int cols, double *Array)
 }
 
 // ***** For (1) WRITE YOUR ARRAY Y HERE *****
+//declare and initialise array Y of required size to hold result of M x N
 double Y[4][4] = {{0}};
 
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 void multiply2Matrices()
 {
     int Row, row, Col, col, x, y;
+    //matrices declared as double to allow fractional numbers
     double matrixA[10][10] {{0}}, matrixB[10][10] {{0}}, products[3] {0};
     double result[10][10] {{0}};
     while (true) {
          printf("enter rows in matrix A\n");
          scanf("%d", &Row);
-          printf("enter columns in matrix A\n");
-          scanf("%d", &Col);
-          printf("enter rows in matrix B\n");
-          scanf("%d", &row);
-           printf("enter columns in matrix B\n");
-          scanf("%d", &col);
-           printf("matrix A has %d rows,and %d columns\n", Row, Col);
-           printf("matrix B has %d rows, and %d columns\n", row, col);
-          if( Col == row) {
-             break;
-           }
-          else {
-              printf("ERROR incompatible matrices\n");
-              printf("please try again\n");
-            }
+         printf("enter columns in matrix A\n");
+         scanf("%d", &Col);
+         printf("enter rows in matrix B\n");
+         scanf("%d", &row);
+         printf("enter columns in matrix B\n");
+         scanf("%d", &col);
+         printf("matrix A has %d rows,and %d columns\n", Row, Col);
+         printf("matrix B has %d rows, and %d columns\n", row, col);
+            //test compatability of entered matrix configurations
+            if( Col == row) {
+                // if multiplyable move on
+                 break;
+
+            } else {            
+                 printf("ERROR incompatible matrices\n");
+                 printf("please try again\n");
+                }
     }
     //fill matrix A
-        for (x = 0; x < Row; x++) {
-             for (y = 0; y < Col; y++) {
-                printf("enter matrix A\n");
-                 printf("enter value for %d%d\n", x,y);
-                 scanf("%lf", &matrixA[x][y]);
-             }            
-        }
+    for (x = 0; x < Row; x++) {
 
-        for (x = 0; x < row; x++) {
-            for (y = 0; y < col; y++) {
-                printf("enter matrix B\n");
-                printf("enter value for %d%d\n", x,y);
+            for (y = 0; y < Col; y++) {        
+                 printf("enter matrix A value for position %d%d\n", x,y);
+                 scanf("%lf", &matrixA[x][y]);
+            }            
+    }
+
+    //fill matrix B
+    for (x = 0; x < row; x++) {
+            for (y = 0; y < col; y++) {            
+                printf("enter matrix B value for %d%d\n", x,y);
                 scanf("%lf", &matrixB[x][y]);
             }
-        }
-
-            for ( int i=0; i < Row; i++) {
-                for ( int j=0; j < col; j++) {
+    }
+    //nested for loops to cycle through the matrix elements
+    for ( int i=0; i < Row; i++) {
+            for ( int j=0; j < col; j++) {
                     for ( int k=0; k < row; k++) {
 
-                        products[k] = matrixA[i][k] * matrixB[k][j];
+                         //multiply row elemnts of A with column elements of B, and write results to array "products"
+                         products[k] = matrixA[i][k] * matrixB[k][j];
 
-                        result[i][j] += products[k];
+                         //write the sum of array "products" into the correct element of matrix array "result"
+                         result[i][j] += products[k];
 
-                        printf("%.2lf x %.2lf = %.2lf\n", matrixA[i][k], matrixB[k][j], products[k]);
-                        wait_us(500000);
-                        printf("sum of row %d x column %d = %.2lf\n", i, k, result[i][j]);
-                        wait_us(500000);
+                         //print arithmetic operations to determine progress, delay is to improve readabilityn (but a large matrix takes a long time)
+                         //this section could be removed for use as a general function
+                         printf("%.2lf x %.2lf = %.2lf\n", matrixA[i][k], matrixB[k][j], products[k]);
+                         wait_us(500000);
+                         printf("sum of row %d x column %d = %.2lf\n", i, k, result[i][j]);
+                         wait_us(500000);
                     }
                 }
             }
-
-            for ( int i=0; i < Row; i++) {
-                 if( i>0) {printf("\n");}
-                for (int j=0; j < col; j++) {
-                   
-
-                    printf("%8.1lf\t", result[i][j]);
-                }
+    //nested loops to print array "result"
+    for ( int i=0; i < Row; i++) {        
+            if( i>0) {
+                //print new line for every row
+                printf("\n");
             }
-            printf("\n"); 
+                    for (int j=0; j < col; j++) {
+                   
+                     printf("%8.1lf\t", result[i][j]);
+                    }
+    }
+    //empty line for general readability of multiple runs
+    printf("\n"); 
 }   
 // *****  END YOUR FUNCTION HERE  *****
 
@@ -105,27 +116,31 @@ int main()
 
     // ***** MODIFY THE CODE BELOW HERE *****
        // 1. Create another Array Y with the correct dimensions to hold the result of M*N (see comments above)
-       //double* addressOfArrayy = (double*)Y;
-      // displayArray(4,4,(double*)Y);
+      
        printf("\n");
        // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
+       //delcare variables, int used for array "product" as only whole numbers are present
        int a, b, c, product[3];
+
+        //nested loops to cycle through matrix elements
         for (a = 0; a < 4; a++) {
-
             for (b = 0; b < 4; b ++){
-
                 for (c = 0; c < 3; c++) {
 
+                    //multiply row of matrix M with column of matrix N and write result to array "product"
                     product[c] = M[a][c] * N[c][b];
 
+                    //sum array and write result into specifyed element of array "Y"
                     Y[a][b] += product[c];
 
                 }
             }
         }
-         displayArray(4,4,(double*)Y);
-
-         multiply2Matrices();
+    //use supplied function to display result matrix Y
+    displayArray(4,4,(double*)Y);
+    
+    //run function to demonstrate it works
+    multiply2Matrices();
 
     
    
